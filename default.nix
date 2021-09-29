ea2b1921dbebcc1089be4d4946d61b9c982f6af1
Daniel Olsen <daniel.olsen99@gmail.com>
2021-09-29 22:33:56 +0200

    Make nix build files

---
+++ default.nix
@@ -0,0 +1,22 @@
+{ pkgs  ? import <nixos> {} }:
+
+let
+  omnikey = pkgs.stdenv.mkDerivation {
+    pname = "read-omnikey";
+    version = "0.1.0";
+
+    buildInputs = with pkgs; [ (perl.withPackages (x: with x; [pcscperl])) ];
+
+    unpackPhase = "true";
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/bin
+      cp ${./read-omnikey.pl} $out/bin/read-omnikey
+      chmod +x $out/bin/read-omnikey
+      runHook postInstall
+    '';
+  };
+in
+{
+  inherit omnikey;
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# kortleser2
+
+
+## Nix
+Get the libraries and script:
+```
+nix-build -A omnikey
+./result/read-omnikey
+```
